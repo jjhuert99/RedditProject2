712da5c585eeaed6e2d3d049033a67694d601f9c
greenstriker99 <imakftw@gmail.com>
2021-10-19 15:05:41 -0500

    added space between recyclerview items

--- app/src/main/java/com/example/redditproject2/homepage/HomeFragment.kt
+++ app/src/main/java/com/example/redditproject2/homepage/HomeFragment.kt
@@ -11,6 +11,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.redditproject2.databinding.HomeFragmentBinding
